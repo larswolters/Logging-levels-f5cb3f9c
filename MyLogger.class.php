2a6c73238ee472f70cd795efc05e487a09b72414
Lars <larstwolters@gmail.com>
2020-03-23 11:17:30 +0100

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -2,8 +2,41 @@
 
 class MyLogger
 {
-    public function log($message)
+    public function log($level, $message)
     {
-        return print_r($message);
+        switch ($level) {
+            case 'warning':
+                $this->warning($message);
+                break;
+            case 'error':
+                $this->error($message);
+                break;
+            case 'info':
+                $this->info($message);
+                break;
+            case 'debug':
+                $this->debug($message);
+                break;
+        }
+    }
+
+    public function warning($message)
+    {
+        print_r('Warning:' . $message);
+    }
+
+    public function error($message)
+    {
+        print_r('Error:' . $message);
+    }
+
+    public function info($message)
+    {
+        print_r('Info:' . $message);
+    }
+
+    public function debug($message)
+    {
+        print_r('Debug:' . $message);
     }
 }
